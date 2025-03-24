ad082bb6ec6cbafa6c759d46a4f0f085619ed7e5
mq7253180 <mq7253180@126.com>
2025-03-24 18:07:03 +0800

    xxx

---
+++ master-service/src/main/java/com/xxx/ServiceInitConfiguration.java
@@ -0,0 +1,10 @@
+package com.xxx;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource(value = {"classpath:application-base.properties", "classpath:application-sensitiveness.properties"})
+@Configuration
+public class ServiceInitConfiguration {
+	
+}
---
+++ merchant-master-controller/src/main/java/com/xxx/MerchantControllerConfiguration.java
@@ -0,0 +1,51 @@
+package com.xxx;
+
+import java.io.Serializable;
+import java.text.MessageFormat;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.quincy.auth.entity.Permission;
+import com.quincy.auth.entity.Role;
+import com.quincy.sdk.AuthServerActions;
+import com.quincy.sdk.o.Menu;
+import com.quincy.sdk.o.User;
+
+@Configuration
+public class MerchantControllerConfiguration {
+	@Bean
+	public AuthServerActions authActions() {
+		return new AuthServerActions() {
+			@Override
+			public void sms(String mobilePhone, String vcode, int expireMinuts) {
+				System.out.println(MessageFormat.format("已通过阿里云短信接口向{0}发送验证码: {1}, 失效时间{2}分钟", mobilePhone, new String(vcode), expireMinuts));
+			}
+
+			@Override
+			public List<Role> findRoles(Long userId) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public List<Permission> findPermissions(Long userId) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public List<Menu> findMenus(Long userId) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public <T extends Serializable> T userExt(User user) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
+}
