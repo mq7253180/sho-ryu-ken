ba69d78ce7665cbf1fc890b5ee394b4160a7c219
mq7253180 <mq7253180@126.com>
2025-03-25 05:40:47 +0800

    xxx

--- master-service/src/main/java/com/xxx/ServiceInitConfiguration.java
+++ master-service/src/main/java/com/xxx/ServiceInitConfiguration.java
@@ -3,7 +3,7 @@ package com.xxx;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource(value = {"classpath:application-base.properties", "classpath:application-sensitiveness.properties"})
+@PropertySource(value = {"classpath:application-other.properties", "classpath:application-sensitiveness.properties"})
 @Configuration
 public class ServiceInitConfiguration {
 	
